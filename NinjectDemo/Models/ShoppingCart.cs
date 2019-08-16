4fd81357abe5c1ab4bd6da89373c3be8b2be85d2
Eugene Lu <qenteen@gmail.com>
2019-08-16 19:41:16 +0300

    Initial commit

---
+++ NinjectDemo/Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NinjectDemo.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator _calc;
+        public IEnumerable<Product> Products { get; set; }
+
+        public ShoppingCart(LinqValueCalculator calculator)
+        {
+            _calc = calculator;
+        }
+
+        public decimal CalculateProductTotal()
+        {
+            return _calc.ValueProducts(Products);
+        }
+    }
+}
